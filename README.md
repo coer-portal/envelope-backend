449dd84625e492915eb2ee58e5bd2fa7f88d5000
Ishan Jain <ishanjain28@gmail.com>
2017-08-24 21:31:55 +0530

    Fixed rate limit code, better error responses, Working on readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # envelope-backend
-Backend Server of Envelope, A Anonymous Text/Photo Posting Website
+
+Envelope is a Anonymous Text/Photo Posting website for College Students. 
+This repository has the code for the server 
+
+## Endpoints
+
+### Submit Text Post
+
+#### Request 
+
+Endpoint
+
+    POST /submit-post
+
+Headers
+
+    Content-Type: application/x-www-form-urlencoded
+
+Body
+
+    text: <The text content of post>
+
--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const police = require('./police/index'),
 
 const app = express();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/colossal',
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/envelope',
 	PORT = process.env.PORT || 5000;
 
 mongo.connect(MONGODB_URI, (err, db) => {
@@ -58,47 +58,70 @@ app.use(bodyParser.urlencoded({
 }));
 
 // Rate limit the following endpoints
-app.use('/submit-post', rateLimit);
+
 app.use('/submit-photo', rateLimit);
 
-app.post('/submit-post', (req, res) => {
+app.post('/submit-post', (req, res, next) => {
+
+	if (req.body && req.body.text) {
+		next();
+	} else {
+		sendErrorResponse(req, res, {
+			message: 'Bad Request',
+			code: 400
+		}, 400);
+	}
+
+}, rateLimit, (req, res) => {
 	const mongoDB = req.app.locals.db;
 	const posts = mongoDB.collection('posts');
 
-	if (req.body && req.body.text) {
-		let shareHash = createShareHash();
-		let editHash = createEditHash();
-		// Time in unix format
-		const time = Math.floor(new Date() / 1000);
+	// Share hash will be used in the link that is generated when user shares a post 
+	// The link will have share_hash in it and when someone visits that page they'll 
+	// see the shared post
+	let shareHash = createShareHash();
+	// Edit hash is generated and given to the original poster
+	// The only possible way to edit the post will be to have the edit link(which contains edit_hash) with you
+	// if you lose the edit link then no one can edit the post
+	let editHash = createEditHash();
+	// Time in unix format
+	const time = Math.floor(new Date() / 1000);
 
-		const md5Hash = createMD5Hash(req.body.text);
+	const md5Hash = createMD5Hash(req.body.text);
 
-		let postdata = {
-			share_hash: shareHash,
-			text: req.body.text,
-			time: time,
-			edit_hash: editHash,
-			md5: md5Hash
-		};
+	let postdata = {
+		share_hash: shareHash,
+		text: req.body.text,
+		time: time,
+		edit_hash: editHash,
+		md5: md5Hash
+	};
 
-		db.save(postdata, posts).then(result => {
-			console.log(`Saved ${result.result.ok} post`);
-			res.status(200).end();
-		}, err => {
-			if (err.code == '11000') {
-				console.error(`duplicate post. md5: ${postdata.md5}, share_hash: ${postdata.share_hash}`);
-				res.status(400).write('DUP');
-				res.end();
-			} else {
-				console.error(`Error occurred in storing post: ${err}`);
-				res.status(500).end();
-			}
+	db.save(postdata, posts).then(result => {
+		console.log(`Saved ${result.result.ok} post`);
+		res.send(JSON.stringify({
+			share_hash: postdata.shareHash,
+			edit_hash: postdata.edit_hash,
+			time: postdata.time,
+		}));
 
-		});
-	} else {
-		res.status(400).write('Bad Request');
-		res.end();
-	}
+	}, err => {
+		if (err.code == '11000') {
+			console.error(`duplicate post. md5: ${postdata.md5}, share_hash: ${postdata.share_hash}`);
+
+			sendErrorResponse(req, res, {
+				message: err.message,
+				code: err.code
+			}, 400);
+
+		} else {
+			console.error(`Error occurred in storing post: ${err}`);
+			sendErrorResponse(req, res, {
+				message: err.message,
+				code: err.code
+			}, 500);
+		}
+	});
 });
 
 function createShareHash() {
@@ -111,4 +134,28 @@ function createEditHash() {
 
 function createMD5Hash(text) {
 	return crypto.createHash('md5').update(text).digest('hex');
+}
+
+function sendErrorResponse(req, res, err, code) {
+	let accepts = req.headers['accept'];
+
+	res.status(code);
+
+	if (accepts.indexOf('*/*') != -1 || accepts.indexOf('/json') != -1) {
+		res.write(JSON.stringify({
+			error: err.message,
+			code: code
+		}));
+	} else {
+		res.write(`<html>
+	<head>
+		<title>Error Occurred</title>
+	</head>
+	<body>
+		<h1>Error Occurred: ${code}</h1>
+		<p>${err.message}<p>
+	</body>
+</html>`);
+	}
+	res.end();
 }
